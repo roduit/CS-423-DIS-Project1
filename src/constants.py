cbc56d74bd2d160105b643036d4a2a260213cac2
Vincent Roduit <vincent.roduit@epfl.ch>
2024-10-03 19:14:28 +0200

    try now

--- src/constants.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# -*- authors : Vincent Roduit -*-
-# -*- date : 2024-09-30 -*-
-# -*- Last revision: 2024-09-30 by Vincent Roduit -*-
-# -*- python version : 3.9.19 -*-
-# -*- Description: Constants used in the code *-
